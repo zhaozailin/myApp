ab260e46e6b2c99234ed099e27de1b7cca82ef02
zhaozl <zhaozl@servyou.com.cn>
2019-04-01 01:00:41 +0800

    首页

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -22,8 +22,13 @@ export default class Main extends Component {
 
   render() {
     return (
-      <View className='index'>
+      <View className='m-wrap'>
         <Text>首页</Text>
+        <View className='m-bottom-wrap'>
+          <Button type='primary' size='mini'>工单管理</Button>
+          <Button type='primary' size='mini' className='m-bottom-btn'>门店管理</Button>
+          <Button type='primary' size='mini' className='m-bottom-btn'>我的</Button>
+        </View>
       </View>
     )
   }
