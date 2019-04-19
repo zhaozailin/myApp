ebd65f32ab33250f567ef79e5b4330b474574ae1
zhaozl <zhaozl@servyou.com.cn>
2019-04-19 23:52:37 +0800

    init

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,88 +4,22 @@ import './index.less'
 
 export default class Index extends Component {
   state = {
-    // 微信授权，1-已授权，0-未授权，-1-空白视图
-    weAuth: -1
   }
 
   config = {
     navigationBarTitleText: '授权'
   }
 
-  onShareAppMessage (res) {
-    // if (res.from === 'button') {
-    //   // 来自页面内转发按钮
-    //   console.log(res.target)
-    // }
-    return {
-      title: '自定义转发标题',
-      path: '/pages/loginRegister/index?id=123',
-      imageUrl: 'http://src.onlinedown.net/images/xcs/4/fbbf04b527486a623eaa33fc8304bbca396afb34-a0dcd7fbbcba9dad42759929b02fdbd52bd03aeb.png',
-      success(r) {
-        console.log(r);
-        wx.showShareMenu({
-          withShareTicket: true
-        })
-      },
-      fail(r) {
-        console.log(r);
-      }
-    }
-  }
-
   componentWillMount() {
-    // 判断用户是否已经授权
-    Taro.getSetting().then((res) => {
-      // 已授权
-      // if (res.authSetting['scope.userInfo']) {
-
-        // 获取登录用户信息
-
-        // 已登录
-        // Taro.redirectTo({
-        //   url: '/pages/main/index'
-        // })
-
-        // 未登录
-        Taro.redirectTo({
-          url: '/pages/loginRegister/index'
-        })
-      // }
-    })
-  }
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  componentDidShow() {
-  }
-
-  componentDidHide() {
-  }
-
-  enter = () => {
-    Taro.getUserInfo().then(() => {
-      Taro.redirectTo({
-        url: '/pages/main/index'
-      })
-    }, () => {
-      Taro.showToast({title: '请先进行授权操作', icon: 'none'})
+    Taro.redirectTo({
+      url: '/pages/loginRegister/index'
     })
   }
 
   render() {
     return (
       <View>
-        {this.state.weAuth === 0 &&
-        <View className='auth-wrap'>
-          <Button className='auth-btn' openType='getUserInfo'>授权</Button>
-          <Text className='auth-info'>如已授权，点击进入按钮</Text>
-          <Button className='auth-enter-btn' type='primary' onClick={this.enter}>点击进入</Button>
-        </View>
-        }
+
       </View>
     )
   }
--- src/pages/toMother/index.js
+++ src/pages/toMother/index.js
@@ -1,14 +1,41 @@
 import Taro, {Component} from '@tarojs/taro'
+import {View, Text, Button} from '@tarojs/components'
 import {getOpenId} from "../../utils/payUtils";
+import './index.less'
 
 export default class ToMother extends Component {
   state = {
     openId: '',
-    shopId: ''
+    shopId: '',
+    nickname: '',
+    // 微信授权，1-已授权，0-未授权，-1-空白视图
+    weAuth: -1
   }
 
   config = {
     navigationBarTitleText: ''
+  }
+
+  componentWillMount() {
+    // 判断用户是否已经授权
+    Taro.getSetting().then((res) => {
+      // 已授权
+      if (res.authSetting['scope.userInfo']) {
+        // 获取登录用户信息
+        Taro.getUserInfo().then((res2) => {
+          this.setState({
+            nickname: res2.userInfo.nickName,
+            weAuth: 1,
+          })
+        })
+      }
+      else {
+        Taro.showToast({title: '请先进行授权操作', icon: 'none'})
+        this.setState({
+          weAuth: 0
+        })
+      }
+    })
   }
 
   componentDidMount() {
@@ -29,10 +56,28 @@ export default class ToMother extends Component {
     })
   }
 
+  enter = () => {
+    Taro.getUserInfo().then((res) => {
+      this.setState({
+        nickname: res.userInfo.nickName,
+        weAuth: 1,
+      })
+    }, () => {
+      Taro.showToast({title: '请先进行授权操作', icon: 'none'})
+    })
+  }
+
   render() {
     return (
       <View>
-        <web-view src={'http://localhost:3000?openId=' + this.state.openId + '&shopId=' + this.state.shopId +'#/main'}/>
+        {this.state.weAuth === 0 &&
+        <View className='auth-wrap'>
+          <Button className='auth-btn' openType='getUserInfo'>授权</Button>
+          <Text className='auth-info'>如已授权，点击进入按钮</Text>
+          <Button className='auth-enter-btn' type='primary' onClick={this.enter}>点击进入</Button>
+        </View>
+        }
+        {this.state.weAuth === 1 && <web-view src={'http://localhost:3000?openId=' + this.state.openId + '&shopId=' + this.state.shopId +'&nickname=' + this.state.nickname + '#/main'}/>}
       </View>
     )
   }
