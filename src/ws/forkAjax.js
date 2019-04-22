cdec6cf636e8c38eb1c780e2dd90addb9be810e6
zhaozl <zhaozl@servyou.com.cn>
2019-04-22 20:16:16 +0800

    init

--- src/ws/forkAjax.js
+++ src/ws/forkAjax.js
@@ -1,13 +1,11 @@
 import {packRequest} from "../utils/request";
 import {createWs} from "./index";
-import Taro from '@tarojs/taro'
 
 export const forkAjax = (requestCode, params, resolves) => {
   return new Promise((res) => {
     let list = resolves.list;
     list['resolve' + requestCode] = res;
     createWs(requestCode).then((task) => {
-      params.globalAuth = Taro.getStorageSync('auth');
       task.send({data: packRequest(params, requestCode)})
     })
   })
