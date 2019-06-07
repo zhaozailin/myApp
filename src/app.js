b4349e234e77f9922b3cdf075de1f5caa3b5f1a4
zhaozl <zhaozl@servyou.com.cn>
2019-06-08 05:18:07 +0800

    改变taro-ui主题

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import Taro, { Component } from '@tarojs/taro'
 import Index from './pages/index/index'
 import './app.less'
-import 'taro-ui/dist/style/index.scss'
+import './custom-theme.scss'
+import './taro-ui.css'
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
