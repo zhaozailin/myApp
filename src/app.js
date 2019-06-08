35e33c063d8cc017869667def7cce11d89558bab
zhaozl <zhaozl@servyou.com.cn>
2019-06-08 09:25:40 +0800

    主题调试

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import Taro, { Component } from '@tarojs/taro'
 import Index from './pages/index/index'
 import './app.less'
 import './custom-theme.scss'
-import './taro-ui.css'
+import './style/index.scss'
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
